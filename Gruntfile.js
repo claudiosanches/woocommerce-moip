ffae58a861ba279b52dc8e5ef2b5767dec4632a4
claudiosmweb <contato@claudiosmweb.com>
2013-07-19 22:59:29 -0300

    added compass and jshint in grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,12 +12,80 @@ module.exports = function(grunt) {
             exclude: [
                 ".git/",
                 ".gitignore",
+                ".sass-cache/",
                 "node_modules/",
+                "assets/sass/",
+                "assets/js/admin.js",
+                "assets/js/checkout.js",
                 "Gruntfile.js",
                 "README.md",
                 "package.json",
+                "config.rb",
                 "*.zip"
             ]
+        },
+
+        // javascript linting with jshint
+        jshint: {
+            options: {
+                "bitwise": true,
+                "eqeqeq": true,
+                "eqnull": true,
+                "immed": true,
+                "newcap": true,
+                "es5": true,
+                "esnext": true,
+                "latedef": true,
+                "noarg": true,
+                "node": true,
+                "undef": false,
+                "browser": true,
+                "trailing": true,
+                "jquery": true,
+                "curly": true
+            },
+            all: [
+                "Gruntfile.js",
+                "assets/js/admin.js",
+                "assets/js/checkout.js"
+            ]
+        },
+
+        // uglify to concat and minify
+        uglify: {
+            dist: {
+                files: {
+                    "assets/js/admin.min.js": ["assets/js/admin.js"],
+                    "assets/js/checkout.min.js": ["assets/js/checkout.js"]
+                }
+            }
+        },
+
+        // compass and scss
+        compass: {
+            dist: {
+                options: {
+                    config: "config.rb",
+                    force: true,
+                    outputStyle: "compressed"
+                }
+            },
+            js: {
+                files: [
+                    "<%= jshint.all %>"
+                ],
+                tasks: ["jshint", "uglify"]
+            }
+        },
+
+        // watch for changes and trigger compass
+        watch: {
+            compass: {
+                files: [
+                    "assets/sass/**"
+                ],
+                tasks: ["compass"]
+            }
         },
 
         // rsync commands used to take the files to svn repository
@@ -62,11 +130,23 @@ module.exports = function(grunt) {
     });
 
     // load tasks
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-compass");
     grunt.loadNpmTasks("grunt-rsync");
     grunt.loadNpmTasks("grunt-shell");
 
+    // compile task
+    grunt.registerTask("compile", [
+        "jshint",
+        "compass",
+        "uglify"
+    ]);
+
     // default task
     grunt.registerTask("default", [
+        "compile",
         "rsync:tag",
         "rsync:trunk",
         "shell:svn_add",
